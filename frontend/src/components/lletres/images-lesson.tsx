23d82baf66f82fcfc7d4d4a549021d74820fa7fa
kippel <kippeld@gmail.com>
2025-10-19 07:21:27 +0200

    svg

--- frontend/src/components/lletres/images-lesson.tsx
+++ frontend/src/components/lletres/images-lesson.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image"
 
 type Props = {
-    vocals_images_bar: string;
+    vocals_images_bar: string | null;
 }
 
 
 export const ImagesLesson = ({ vocals_images_bar} : Props) => {
     return (
         <>
+                    
         { vocals_images_bar && (
+
                         <Image
                         src={vocals_images_bar}
                         width={500}
